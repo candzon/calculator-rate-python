0d109781874aba411d07caf3547bb028b891912b
candzon <mf123cw@gmail.com>
2022-11-03 13:19:36 +0700

    calculator.py

---
+++ calculator.py
@@ -0,0 +1,16 @@
+hrs = input("Enter Hours: ")
+rphrs = input("Enter Rate: ")
+
+try:
+    irphrs = float(rphrs)
+    ihrs = int(hrs)
+
+except:
+    irphrs = -1
+    ihrs = -1
+
+if irphrs > 0.0 and ihrs > 0 :
+    pay: float = float(hrs) * float(rphrs)
+    print(pay)
+else:
+    print("Input Not a Number")
